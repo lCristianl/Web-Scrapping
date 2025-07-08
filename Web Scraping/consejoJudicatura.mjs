ab68d34b659afd32934dd0e5f588bde3767b0b0c
lCristianl <cjortega03@tecnologicosudamericano.edu.ec>
2025-07-08 17:58:56 -0500

    Nuevas paginas
    se aladieron nuevas paginas de scraping, la de consejoJudicatura falta verificar que funcione correctamente.

---
+++ Web Scraping/consejoJudicatura.mjs
@@ -0,0 +1,29 @@
+import { chromium } from "playwright"
+
+const browser = await chromium.launch({ headless: true })
+const page = await browser.newPage()
+
+await page.goto("https://www.funcionjudicial.gob.ec/autoridades/", {
+    waitUntil: "domcontentloaded"
+})
+
+try {
+
+    //FALTA AREGLAR LA EXTRACCION DE DATOS
+    const resultados = await page.$$eval(".elementor-widget-container", (element) => {
+        return element.map((el) => {
+            return {
+                nombre: el.querySelector(".elementor-heading-title")?.innerText.trim() || "",
+                autoridad: el.querySelector(".elementor-heading-title")?.innerText.trim() || "",
+            }
+        })
+    })    
+
+  console.log("\nConsejo de la Judicatura:")
+  console.log(resultados)
+
+} catch (error) {
+    console.error("\n❌ No se encontraron resultados.")
+}
+
+await browser.close()
---
+++ Web Scraping/sriConsulta.mjs
@@ -0,0 +1,52 @@
+import { chromium } from "playwright"
+
+const browser = await chromium.launch({ headless: false })
+const page = await browser.newPage()
+
+await page.goto("https://srienlinea.sri.gob.ec/sri-en-linea/SriRucWeb/ConsultaRuc/Consultas/consultaRuc", {
+  waitUntil: "domcontentloaded"
+})
+
+try {
+  //Espera hasta que aparaezca la etiqueta que tiene la clase row
+  await page.waitForSelector("sri-mostrar-contribuyente", { timeout: 60000 })
+  await page.click(".ui-button.cyan-btn.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only")
+    await page.waitForSelector("sri-listar-establecimientos", { timeout: 60000 })
+
+  //
+  const estado = await page.$$eval("sri-mostrar-contribuyente", (elementos) => {
+      return elementos.map((el) => {
+        const estadoSpan = el.querySelector(".col-sm-8.alineacion-texto-centro.titulo-consultas-1.tamano-defecto-campos.alinear-izquierda.ng-star-inserted span")
+        const tds = el.querySelectorAll(".col-sm-12.centrar-texto-tabla td")
+
+          return {
+                estado: estadoSpan?.innerText.trim() || "",
+                tipoContribuyente: tds[0]?.innerText.trim() || "",
+                regimen: tds[1]?.innerText.trim() || "",
+          }
+      })
+  })
+
+  const establecimientos = await page.$$eval(".ui-datatable-data.ui-widget-content tr", (filas) => {
+      return filas.map((fila) => {
+            const columnas = fila.querySelectorAll("td")
+            return {
+                numEstablecimiento: columnas[0]?.innerText.trim() || "",
+                nombre: columnas[1]?.innerText.trim() || "",
+                ubicacion: columnas[2]?.innerText.trim() || "",
+                estado: columnas[3]?.innerText.trim() || "",
+            }
+        })
+    })
+
+
+  console.log("\nRegistros encontrados:")
+  console.log(estado)
+    console.log("\nEstablecimientos encontrados:")
+  console.log(establecimientos)
+
+} catch (error) {
+  console.error("\n❌ No se encontraron resultados. Verifica que los datos ingresados fueron correctos.")
+}
+
+await browser.close()
